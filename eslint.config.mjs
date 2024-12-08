5770a06d10361a0e13406d815b2eb9e103d91bf6
vchikalkin <djchikalkin@gmail.com>
2024-12-08 14:20:52 +0300

    packages: add eslint config

---
+++ eslint.config.mjs
@@ -0,0 +1,44 @@
+import awesome from '@vchikalkin/eslint-config-awesome';
+import tailwind from 'eslint-plugin-tailwindcss';
+
+/**
+ * @type {import("eslint").Linter.Config}
+ */
+export default [
+  ...tailwind.configs['flat/recommended'],
+  ...awesome['react-typescript'],
+  {
+    ignores: ['**/.next/**'],
+    rules: {
+      '@next/next/no-duplicate-head': 'off',
+      'import/extensions': [
+        'error',
+        'ignorePackages',
+        {
+          css: 'always',
+        },
+      ],
+      'prettier/prettier': [
+        'error',
+        {
+          arrowParens: 'always',
+          bracketSameLine: false,
+          bracketSpacing: true,
+          endOfLine: 'auto',
+          insertPragma: false,
+          jsxSingleQuote: false,
+          printWidth: 100,
+          proseWrap: 'preserve',
+          quoteProps: 'as-needed',
+          requirePragma: false,
+          semi: true,
+          singleQuote: true,
+          tabWidth: 2,
+          trailingComma: 'all',
+          useTabs: false,
+        },
+        { usePrettierrc: false },
+      ],
+    },
+  },
+];
