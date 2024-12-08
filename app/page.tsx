4ee687768c512d34f459c9788ab89013aac01267
vchikalkin <djchikalkin@gmail.com>
2024-12-08 20:59:38 +0300

    add metadata

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,22 @@ import { Contacts } from '@/components/contacts';
 import { Person } from '@/components/person';
 import { Skills } from '@/components/skills';
 import { Work } from '@/components/work';
+import { type Metadata } from 'next';
+import { getTranslations } from 'next-intl/server';
+
+type Parameters = {
+  params: {
+    locale: string;
+  };
+};
+
+export async function generateMetadata({ params: { locale } }: Parameters): Promise<Metadata> {
+  const t = await getTranslations({ locale, namespace: 'HomePage' });
+
+  return {
+    title: t('title'),
+  };
+}
 
 export default function HomePage() {
   return (
