c5261634c2bdd6c46551b5badc0fcd91d9140d3d
vchikalkin <djchikalkin@gmail.com>
2024-12-08 20:22:14 +0300

    fix skills title

--- components/skills.tsx
+++ components/skills.tsx
@@ -1,4 +1,5 @@
 import { Badge } from './ui/badge';
+import { useTranslations } from 'next-intl';
 
 const skills = [
   'React',
@@ -16,10 +17,11 @@ const skills = [
 ];
 
 export function Skills() {
+  const t = useTranslations('Skills');
   return (
     <section id="skills">
       <div className="flex min-h-0 flex-col gap-y-3">
-        <h2 className="text-xl font-bold">Skills</h2>
+        <h2 className="text-xl font-bold">{t('title')}</h2>
         <div className="flex flex-wrap gap-1">
           {skills.map((skill) => (
             <Badge key={skill}>{skill}</Badge>
