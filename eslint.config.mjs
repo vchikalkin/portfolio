28570faad3de8175c0e31c9874d35136dd2a0cee
vchikalkin <djchikalkin@gmail.com>
2024-12-09 15:13:27 +0300

    packages: upgrade @vchikalkin/eslint-config-awesome

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,39 +9,5 @@ export default [
   ...awesome['react-typescript'],
   {
     ignores: ['**/.next/**'],
-    rules: {
-      '@next/next/no-duplicate-head': 'off',
-      'canonical/filename-match-regex': 0,
-      'import/extensions': [
-        'error',
-        'never',
-        {
-          css: 'always',
-          json: 'always',
-        },
-      ],
-      'prettier/prettier': [
-        'error',
-        {
-          arrowParens: 'always',
-          bracketSameLine: false,
-          bracketSpacing: true,
-          endOfLine: 'auto',
-          insertPragma: false,
-          jsxSingleQuote: false,
-          printWidth: 100,
-          proseWrap: 'preserve',
-          quoteProps: 'as-needed',
-          requirePragma: false,
-          semi: true,
-          singleQuote: true,
-          tabWidth: 2,
-          trailingComma: 'all',
-          useTabs: false,
-        },
-        { usePrettierrc: false },
-      ],
-      'react/forbid-component-props': 0,
-    },
   },
 ];
